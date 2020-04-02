8e0b498ec2b183f1c91f2c421e2693b167916572
shootface <31258676+shootface@users.noreply.github.com>
2020-04-01 19:04:36 -0500

    Bases de datos lista

--- database.sql
+++ database.sql
@@ -1,16 +1,25 @@
 CREATE TABLE  Producto
 (
-	PK_PRODUCTO NUMBER(8) NOT NULL,    -- Columna que almacena la llave primaria del conjunto de entidades producto de los diferentes productos a vender en la empresa NatAme
-	C_NOMBRE VARCHAR2(30) NOT NULL,    -- Nombre del producto a vender
-	K_SUBCATEGORIA NUMBER(8) NOT NULL,	-- Subcategoria a la que pertenece el producto
-	V_NOMBREIMAGEN VARCHAR2(300), 	-- Imagen para cargar dinámicamente donde encontrar la imagen del producto
-    N_CANTIDAD NUMBER(8) NOT NULL    -- Cantidad del producto
+	PK_PRODUCTO integer NOT NULL,    -- Columna que almacena la llave primaria del conjunto de entidades producto de los diferentes productos a vender en la empresa NatAme
+	C_NOMBRE VARCHAR(30) NOT NULL,    -- Nombre del producto a vender
+	K_SUBCATEGORIA integer NOT NULL,	-- Subcategoria a la que pertenece el producto
+	V_NOMBREIMAGEN VARCHAR(300), 	-- Imagen para cargar dinámicamente donde encontrar la imagen del producto
+    N_CANTIDAD integer NOT NULL    -- Cantidad del producto
 );
+
+ALTER TABLE  Producto 
+ ADD CONSTRAINT PK_PRODUCTO
+	PRIMARY KEY (PK_PRODUCTO) 
+;
 
 CREATE TABLE Usuario
 (
-    PK_CEDULA NUMBER(15) NOT NULL,
+    PK_CEDULA integer NOT NULL,
     C_NOMBRE VARCHAR(30) NOT NULL,
-    
-)
+    C_PASS VARCHAR(30) NOT NULL
+);
 
+ALTER TABLE  Usuario 
+ ADD CONSTRAINT PK_Usuario
+	PRIMARY KEY (PK_cedula) 
+;
